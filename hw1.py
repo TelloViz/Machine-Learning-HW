2ee0087a3d33d9e2b88a34165c66ab6aeed92a8e
Tello- <tellovisionstudio@gmail.com>
2023-08-28 15:49:53 -0700

    add hw 1

---
+++ hw1.py
@@ -0,0 +1,34 @@
+# Building a simple neural network
+
+# Importing the necessary libraries
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# Loading the dataset
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Preprocessing the data
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# Building the model
+model = Sequential([
+    Flatten(input_shape=(28,28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compiling the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training the model
+model.fit(train_images, train_labels, epochs=5, batch_size=32)
+
+# Evaluating the model
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
